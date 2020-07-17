6a50cae9ab6ddf69f07b014a18bff33b1fbb84b9
Michael Bosello <michael.bosello@studio.unibo.it>
2020-07-17 13:32:21 +0200

    remove unused files

--- src/agt/rl/algorithm/tf/Dqn.java
+++
@@ -1,9 +0,0 @@
-package rl.algorithm.tf;
-
-public class Dqn extends TensorFlowAgent{
-	public Dqn(String goal) {
-		super(goal);
-	}
-
-	protected String getMethod() { return "dqn"; }
-}
--- src/agt/rl/algorithm/tf/Reinforce.java
+++
@@ -1,9 +0,0 @@
-package rl.algorithm.tf;
-
-public class Reinforce extends TensorFlowAgent{
-	public Reinforce(String goal) {
-		super(goal);
-	}
-
-	protected String getMethod() { return "reinforce"; }
-}
